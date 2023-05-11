8770698f8efbc1aad8471f01259044e92931c59e
rodiongavrilov <rodiongavrilov@gmail.com>
2023-05-11 14:44:22 +0400

    init

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+const user = useSupabaseUser();
+</script>
+
+<template>
+  <div class="container" style="padding: 50px 0 100px 0">
+    <profile v-if="user" />
+    <login v-else />
+  </div>
+</template>
