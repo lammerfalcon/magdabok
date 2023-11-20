33ae20d73b79414c82eb5959f827e6f3e14e31c8
rodiongavrilov <rodiongavrilov@gmail.com>
2023-11-20 15:14:43 +0300

    init

--- app.vue
+++ app.vue
@@ -1,10 +1,10 @@
 <script setup>
 useSeoMeta({
-  title: 'My Amazing Site',
-  ogTitle: 'My Amazing Site',
+  title: 'Magdabok app',
+  ogTitle: 'Magdabok app',
   description: 'This is my amazing site, let me tell you all about it.',
-  ogDescription: 'This is my amazing site, let me tell you all about it.',
-  ogImage: 'https://example.com/image.png',
+  ogDescription: 'Personal diary for self care',
+  ogImage: 'https://cdn.dribbble.com/userupload/11333665/file/original-16aa0b08aee4b06667b46c4d200c4f52.jpg?resize=2048x1691',
   twitterCard: 'summary_large_image',
 })
 </script>
